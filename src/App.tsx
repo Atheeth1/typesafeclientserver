99012b7182902877a6fcafc8edeb206b5fb85330
Atheeth1 <atheeth.iccc@gmail.com>
2024-04-25 14:24:36 +0530

    with code app correction

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ import { GetUsersDocument } from './graphql/generated'
       query: GetUsersDocument
     })
   
-  // const users: User[] = [{
-  //   name: 'Prisma Fan',
-  //   messages: [{
-  //     body: 'Prisma rocks!!'
-  //   }, {
-  //     body: 'Did I mention I love Prisma?'
-  //   }]
-  // }]
+  const users: User[] = [{
+    name: 'Prisma Fan',
+    messages: [{
+      body: 'Prisma rocks!!'
+    }, {
+      body: 'Did I mention I love Prisma?'
+    }]
+  }]
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
       {
